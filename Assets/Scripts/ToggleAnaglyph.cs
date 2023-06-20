03dd613a359ea9fd719aeb4004d2184ba097ef53
Nick Fox-Gieg <nick@fox-gieg.com>
2023-06-20 14:43:24 -0400

    first

---
+++ Assets/Scripts/ToggleAnaglyph.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleAnaglyph : MonoBehaviour {
+
+	private void Start() {
+		
+	}
+
+	/*
+	 public s3dCamera anaglyph3d;
+
+	void Awake() {
+		if (anaglyph3d == null) anaglyph3d = GetComponent<s3dCamera>();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Alpha3)) {
+			if (!anaglyph3d.enabled) { 
+				anaglyph3d.enabled = true; 
+			} else {
+				anaglyph3d.enabled = false; 
+			}
+		}
+	}
+	*/
+
+}
